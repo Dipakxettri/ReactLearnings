19632f7555df81ebb7b98071446d1b55c1b69e7d
deepakghimire <codeepakghimire@gmail.com>
2025-11-27 17:06:07 +0545

    React is just op and fun

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const userID = 12;
 
   return (
     <div>
-      <p>Hello this is my persional site</p>
+      <p>Hello this is my persional site {userID}</p>
+      {/*anything written under { } in html tag is eviluation expression it means it takes only final output  */}
+      
     </div>
+
+    // userID -> this is invalid 
     
   )
 }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,4 +1,5 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react' -> cannot import strict mode two times
+import React, { StrictMode } from 'react'; // additional import for dev 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Hello from './Hello.jsx'
@@ -12,16 +13,40 @@ function MyApp(){
   )
 }
 
-// just trying to give react a object directally - Error
-const reactElement = {
-    type : 'a',
-    props: {
-        href: 'htts://google.com',
-        target: '_blank',
 
-    },
-    children: "click me to visit google"
-}
+// just trying to give react a object directally - Error
+// const reactElement = {
+//     type : 'a',
+//     props: {
+//         href: 'htts://google.com',
+//         target: '_blank',
+
+//     },
+//     children: "click me to visit google"
+// }
+
+
+// but this will work
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>DO visit</a>
+)
+
+
+
+let userID = 20;
+// ------- Lets see how react works create elements under the hood -------
+const newReactElement = React.createElement(
+  // The syntax is pre defined not like in our custom react where we use our own syntax
+
+  'a', // actual tag
+  {hrefs: "href://google.com", target: "_blank"}, // This add proproties
+  'click me', // Text 
+
+  userID //This works
+)
+
+
+
 
 
 createRoot(document.getElementById('root')).render(
@@ -30,12 +55,19 @@ createRoot(document.getElementById('root')).render(
 
 
     <App /> 
-    
+
     {/* <Hello /> -> this is actually a function can be call as Hello() */}
 
     <MyApp />
 
     {/* reactElement -> here this will return an error as react is expecting valid jsx element but we gave object directly */}
+
+    {AnotherElement}
+    {/* {AnotherElement} -> this works it use Babel*/}
+
+
+    {/* lets give that react element */}
+    {newReactElement}
 
   </StrictMode>
 
